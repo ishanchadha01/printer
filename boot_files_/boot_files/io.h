f9d12d15af132f36c55971296f46131c1f22db04
ishan <ishanchadha01@gmail.com>
2025-01-03 21:28:06 -0800

    wiringPi needs to be updated for mac build, or can use circle, or logic analyzer to check pi pin outputs

---
+++ boot_files_/boot_files/io.h
@@ -0,0 +1,15 @@
+void uart_init();
+void uart_write_text(char* buffer);
+void uart_write_byte_blocking_actual(unsigned char ch);
+void mmio_write(long reg, unsigned int val);
+unsigned int mmio_read(long reg);
+
+enum {
+    GPIO_MAX_PIN       = 27,
+    GPIO_FUNCTION_OUT  = 1,
+    GPIO_FUNCTION_ALT0 = 4,
+};
+
+unsigned int gpio_set     (unsigned int pin_number, unsigned int value);
+unsigned int gpio_clear   (unsigned int pin_number, unsigned int value);
+unsigned int gpio_function(unsigned int pin_number, unsigned int value);
