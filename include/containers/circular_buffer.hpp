cc82ac7cb26b4f384a2e4ba3b91feee534b99f30
ishan <ishanchadha01@gmail.com>
2025-06-22 15:20:27 -0700

    task buffer

---
+++ include/containers/circular_buffer.hpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <mutex>
+#include <array>
+#include <optional>
+
+template<typename T, size_t BufferSize>
+class CircularBuffer
+{
+public:
+    CircularBuffer()
+    {
+        this->head = 0;
+        this->tail = 0;
+    };
+
+    bool emplace(T&& data)
+    {
+        std::scoped_lock rw_lock(_rw_mutex);
+        if ((((this->tail)+1) % BufferSize) == (this->head)) {
+            std::cerr << "Buffer is full!\n";
+            return false;
+        }
+        _data[this->tail] = std::move(data);
+        this->tail = ((this->tail)+1) % BufferSize;
+        return true;
+    }
+
+    bool push(const T& data)
+    {
+        std::scoped_lock rw_lock(_rw_mutex);
+        if ((((this->tail)+1) % BufferSize) == (this->head)) {
+            std::cerr << "Buffer is full!\n";
+            return false;
+        }
+        _data[this->tail] = data;
+        this->tail = ((this->tail)+1) % BufferSize;
+        return true;
+    }
+
+    std::optional<T> pop()
+    {
+        std::scoped_lock rw_lock(_rw_mutex);
+        if (this->head == this->tail) {
+            std::cerr << "Buffer is empty!\n";
+            return std::nullopt;
+        }
+        auto& old_front = _data[this->head];
+        this->head = ((this->head)+1) % BufferSize;
+        return std::move(old_front);
+    }
+    
+
+private:
+    std::array<T, BufferSize> _data;
+    size_t head;
+    size_t tail;
+    std::mutex _rw_mutex;
+};
