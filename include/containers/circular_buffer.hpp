2c539e0d8d2a916e7469d8deb23b41d54b0ceeee
ishan <ishanchadha01@gmail.com>
2025-06-30 21:58:44 -0700

    fixes to circular buffer with internal cv

--- include/containers/circular_buffer.hpp
+++ include/containers/circular_buffer.hpp
@@ -14,42 +14,48 @@ public:
     {
         this->head = 0;
         this->tail = 0;
+        this->size = 0;
+        this->capacity = BufferSize;
     };
 
     bool emplace(T&& data)
     {
         std::scoped_lock rw_lock(_rw_mutex);
-        if ((((this->tail)+1) % BufferSize) == (this->head)) {
-            std::cerr << "Buffer is full!\n";
-            return false;
-        }
+        buffer_full_cv.wait(rw_lock, [this] {
+            return this->size < this->capacity;
+        });
         _data[this->tail] = std::move(data);
         this->tail = ((this->tail)+1) % BufferSize;
+        size++;
+        buffer_empty_cv.notify_one();
         return true;
     }
 
     bool push(const T& data)
     {
         std::scoped_lock rw_lock(_rw_mutex);
-        if ((((this->tail)+1) % BufferSize) == (this->head)) {
-            std::cerr << "Buffer is full!\n";
-            return false;
-        }
+        buffer_full_cv.wait(rw_lock, [this] {
+            return this->size < this->capacity;
+        });
         _data[this->tail] = data;
         this->tail = ((this->tail)+1) % BufferSize;
+        size++;
+        buffer_empty_cv.notify_one();
         return true;
     }
 
     std::optional<T> pop()
     {
         std::scoped_lock rw_lock(_rw_mutex);
-        if (this->head == this->tail) {
-            std::cerr << "Buffer is empty!\n";
-            return std::nullopt;
-        }
+        buffer_empty_cv.wait(rw_lock, [this] {
+            return this->size > 0;
+        });
         auto& old_front = _data[this->head];
         this->head = ((this->head)+1) % BufferSize;
-        return std::move(old_front);
+        size--;
+        buffer_full_cv.notify_one();
+        T result = std::move(old_front);
+        return result;
     }
 
     std::optional<std::reference_wrapper<T>> poll()
@@ -65,14 +71,14 @@ public:
     std::optional<T> pop_if(std::function<bool(const T&)> cond_func)
     {
         std::scoped_lock rw_lock(_rw_mutex);
-        if (this->head == this->tail) {
-            std::cerr << "Buffer is empty!\n";
-            return std::nullopt;
-        }
+        buffer_empty_cv.wait(rw_lock, [this] {
+            return size > 0;
+        });
         if (!cond_func(_data[this->head])) return std::nullopt;
-
         T result = std::move(_data[this->head]);
         this->head = (this->head + 1) % BufferSize;
+        size--;
+        buffer_full_cv.notify_one();
         return result;
     }
     
@@ -81,5 +87,9 @@ private:
     std::array<T, BufferSize> _data;
     size_t head;
     size_t tail;
+    size_t size;
+    size_t capacity;
     std::mutex _rw_mutex;
+    std::condition_variable buffer_full_cv;
+    std::condition_variable buffer_empty_cv;
 };
